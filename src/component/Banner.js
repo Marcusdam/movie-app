bae9d8ac94a8f4c5753cb67670ca818809fa1c48
Marcus <damilolamarcus1@gmail.com>
2024-09-06 19:09:46 +0100

    update

--- src/component/Banner.js
+++ src/component/Banner.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-// import requests from '../requests';
 
 const Banner = () => {
 
@@ -17,19 +16,20 @@ const Banner = () => {
  
   return (
    <div>
-     <header className='bg-cover bg-center h-[100vh] w-full p-20 object-fill shadow-md shadow-white'
+     <header className='bg-cover bg-center h-[100vh] w-full  object-fill shadow-md shadow-white'
     style={{
         backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`
      
     }}>
-      <div className=' mt-20 p-4 space-y-6 max-w-md text-white' >
-        <h2 className='text-white text-3xl mt-10 font-bold'>{movie?.title || movie?.name || movie?.original_name}</h2>
-        <div className='grid grid-cols-2'>
-        <button className='px-6 py-2 bg-black opacity-80 rounded text-white mr-4 font-bold hover:bg-red-500' type='button' >Play</button>
-        <button className='px-6 py-2 bg-black opacity-80 rounded text-white mx-4 font-bold hover:bg-red-500' type='button'>My List</button>
+      <div className=' pt-28 px-10 space-y-6 w-full md:max-w-md text-white' >
+        <h2 className='text-white text-6xl mt-10 font-bold'>{movie?.title || movie?.name || movie?.original_name}</h2>
+        <div className='grid grid-cols-2 gap-5'>
+        <button className='px-4 py-2  bg-black opacity-80 rounded text-white font-bold hover:bg-red-500' type='button' >Play</button>
+        <button className='px-4 py-2 bg-black opacity-80 rounded text-white  font-bold hover:bg-red-500' type='button'>My List</button>
+   
         </div>
         <div>
-          <h1 className='text-sm'>{movie.overview}</h1>
+          <p className='text-md'>{movie.overview}</p>
         </div>
 
 
